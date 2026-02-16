0c9cd73d5980bd2ed4c9bbadcd1f75cd69270a1b
fredieposh <maor1234@gmail.com>
2026-02-16 20:57:15 +0200

    Style post inners

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,18 @@
 import { useState, useEffect } from "react";
 
+function convertMessagesTimeFormat (post) {
+        const rawDate = new Date(post?.createdAt);
+        const formattedDate = new Intl.DateTimeFormat('en-GB', {
+            hour: '2-digit',
+            minute: '2-digit',
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour12: false
+        }).format(rawDate).replace(',', '');
+        return formattedDate;
+};
+
 function Posts(){
     const [posts, setPosts] = useState(null);
     const [isAuth, setIsAuth] = useState(false);
@@ -19,20 +32,25 @@ function Posts(){
     },[]);
 
     return (
-        <div className="mt-8">
+        <div className="flex justify-center mt-8">
             {
                 posts ?
-                posts.map(post => 
-                    <Card 
-                        key={post.id}
-                        id={post.id}
-                        postTitle={post.postTitle}
-                        postContent={post.postContent}
-                        createdAt={post.createdAt}
-                        isPublished={post.isPublished}
-                        comments={post.comments}
-                    />):
-                <div>No posts to display</div>
+                (
+                    posts.length > 0 ?
+                    posts.map(post => 
+                        <Card 
+                            key={post.id}
+                            id={post.id}
+                            postTitle={post.postTitle}
+                            postContent={post.postContent}
+                            createdAt={convertMessagesTimeFormat(post)}
+                            isPublished={post.isPublished}
+                            comments={post.comments}
+                        />):                    
+                        <div>No posts to display</div>
+                ):
+                <div className="flex flex-col items-center gap-3">Loading Posts</div>
+
             }
         </div>
     )
@@ -48,10 +66,10 @@ function Card({
 }) {
 
     return (
-        <div className="flex flex-col items-center gap-3">
+        <div className="flex flex-col items-left gap-3 border-1 border-slate-400 rounded-lg p-4 md:w-[40%] ">
             <div className="font-bold text-xl">{postTitle}</div>
-            <div>{postContent}</div>
-            <div className="flex">
+            <div className="mt-3">{postContent}</div>
+            <div className="flex justify-end gap-4 text-sm mt-6" >
                 <div>{createdAt}</div>
                 <div>Post Comments: {comments.length}</div>
             </div>
