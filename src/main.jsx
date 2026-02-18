f5e62f5be0905e062e038509a830926bdc2ad95f
fredieposh <maor1234@gmail.com>
2026-02-18 23:50:39 +0200

    Add routing system

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import Posts from './components/Posts.jsx'
+import Sign from './components/Sign.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {index: true, element: <Posts />},
+      {path: "/sign", element: <Sign />},
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
