88aeddf1b0a01656ee66c9702cb967401e596538
fredieposh <maor1234@gmail.com>
2026-02-21 13:01:35 +0200

    Add error page

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,13 @@ import App from './App.jsx'
 import Posts from './components/Posts.jsx'
 import Sign from './components/Sign.jsx'
 import Login from './components/Login.jsx'
+import ErrorPage from './components/ErrorPage.jsx'
 
 const routes = [
     {
       path: "/",
       element: <App />,
+      errorElement: <ErrorPage />,
       children: [
         {index: true, element: <Posts />},
         {path: "/sign", element: <Sign />},
