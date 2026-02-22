39a3bd8cf14974bee35ca4a4ab4092a4cab4ea8d
fredieposh <maor1234@gmail.com>
2026-02-22 23:23:00 +0200

    Enable commenting for signed users

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { convertMessagesTimeFormat } from "../utils";
 import { Link, useParams, useOutletContext } from "react-router";
 import { Editor } from "@tinymce/tinymce-react";
+import DOMPurify from "dompurify";
 
 function Posts(){
     const [posts, setPosts] = useState(null);
     const [showCommentEditor, setShowCommentEditor] = useState(false);
+    const [showComments, setShowComments] = useState(false);
     const { id } = useParams();
     const { user, isAuth } = useOutletContext(); 
 
@@ -24,7 +26,7 @@ function Posts(){
         };
 
         fetchData();
-    },[]);
+    },[showCommentEditor]);
 
     return (
         <div className="flex justify-center mt-8 ">
@@ -43,8 +45,12 @@ function Posts(){
                             comments={post.comments}
                             isFull={id ? true : false}
                             isAuth={isAuth}
+                            userId={user?.id}
                             toggleCommentEditor={toggleCommentEditor}
+                            setShowCommentEditor={setShowCommentEditor}
                             showCommentEditor={showCommentEditor}
+                            showComments={showComments}
+                            setShowComments={setShowComments}
                         />):                    
                         <div>No posts to display</div>
                 ):
@@ -58,16 +64,19 @@ function Posts(){
 function Card({
     id, 
     postTitle, 
+    showComments,
+    setShowComments,
     postContent, 
     createdAt, 
     isPublished,
+    setShowCommentEditor,
     comments,
+    userId=null,
     isAuth = false,
     isFull = false,
     toggleCommentEditor,
     showCommentEditor
 }) {
-    const [showComments, setShowComments] = useState(false);
     console.log(showCommentEditor);
     return (
         <div className={`flex flex-col items-left gap-3 border-1 border-slate-400 bg-slate-100 rounded-lg p-4 w-[90%] md:w-[60%] ${isFull ? 'mb-3': ''}`}>
@@ -100,44 +109,72 @@ function Card({
                 </>
             )}
             {isAuth && showCommentEditor && (
-                <CommentEditor />
+                <CommentEditor 
+                postId={id}
+                userId={userId}
+                setShowCommentEditor={setShowCommentEditor}/>
             )}
         </div>
     )
 }
 
-function Comment({ commentContent, createdAt }) {
+function Comment({ commentContent, createdAt, setShowCommentEditor }) {
     return (
         <div>
-            <div className="whitespace-pre-wrap">{commentContent}</div>
+            <div className="whitespace-pre-wrap" dangerouslySetInnerHTML={{ __html: commentContent }}></div>
             <div className="text-sm text-slate-500"><span className="font-bold">Posted on:</span> {createdAt}</div>
         </div>        
     )
 };
 
-function CommentEditor({}) {
+function CommentEditor({ postId, userId, setShowCommentEditor }) {
+    const editorRef = useRef(null);
+
+    const postComment = async() => {
+        if (editorRef.current) {
+            const comment = DOMPurify.sanitize(editorRef.current.getContent());
+            if (!comment) {
+                return;
+            }
+            const response = await fetch(`http://localhost:3000/users/${userId}/posts/${postId}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "authorization": `Bearer ${localStorage?.token}`
+                },
+                body: JSON.stringify({ comment }),
+            });
+            const result = await response.json();
+            console.log(`comment posted: ${JSON.stringify(result)}`);
+            setShowCommentEditor(false);
+        }
+    };
+
     return (
         <>
-      <Editor
-        apiKey={import.meta.env.VITE_TINYMCE_API_URL}
-        // onInit={ (_evt, editor) => editorRef.current = editor }
-        // initialValue="<p>This is the initial content of the editor.</p>"
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
-            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount',
-          ],
-          placeholder: 'Add a comment...',
-          toolbar: 'undo redo | blocks | ' +
-            'bold italic forecolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-      />
+            <Editor
+                apiKey={import.meta.env.VITE_TINYMCE_API_URL}
+                onInit={ (_evt, editor) => editorRef.current = editor }
+                // initialValue="<p>This is the initial content of the editor.</p>"
+                init={{
+                height: 500,
+                menubar: false,
+                auto_focus: true,
+                plugins: [
+                    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
+                    'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                    'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount',
+                ],
+                placeholder: 'Add a comment...',
+                autofocus: true,
+                toolbar: 'undo redo | ' +
+                    'bold italic forecolor | alignleft aligncenter ' +
+                    'alignright alignjustify | bullist numlist outdent indent | ' +
+                    'removeformat | help',
+                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                }}
+            />
+            <button className="bg-slate-700 hover:cursor-pointer hover:bg-slate-500 transition-all duration-300 text-white px-4 py-2 rounded-md" onClick={postComment}>Add Comment</button>
         </>
     )
 }
