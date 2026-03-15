26ec178d628db4f34623bb951543884e17479572
fredieposh <maor1234@gmail.com>
2026-03-15 23:57:44 +0200

    Create link to the second client app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router'
 
 const linkStyle = "px-4 py-2 hover:bg-white rounded-lg hover:text-slate-900 hover:cursor-pointer hover:scale-110 transition-all duration-600";
 
-function Navbar({ isAuth, onLogout }) {
+function Navbar({ isAuth, onLogout, user }) {
     return (
         <div className="flex bg-slate-900 items-center justify-between px-8 py-4 text-white">
             <div className="text-2xl font-bold"><Link to="/">Science Blog</Link></div>
             <div className="flex gap-3 text-md">
                 {isAuth ?
                     <>
-                        <span className={linkStyle}>Profile</span>
+                        <Link to={`http://localhost:5174/users/${user.id}/home`} className={linkStyle}>Profile</Link>
                         <span className={linkStyle} onClick={onLogout}>Log Out</span>
                     </>:
                     <div>
